57cccbf352502d416165157075cbf611d500a521
Emran Hasan <phpfour@gmail.com>
2012-08-16 16:39:47 +0600

    Place search now within 2kms. Returned by distance.

--- service/src/Service/Venue/GooglePlaces.php
+++ service/src/Service/Venue/GooglePlaces.php
@@ -19,11 +19,9 @@ class GooglePlaces extends Base
 
         if (!is_null($keyword)) {
             $params['keyword'] = $keyword;
-            $params['rankby'] = 'distance';
-        } else {
-            $params['radius'] = '50000';
         }
 
+        $params['radius'] = '2000';
         $params['sensor'] = 'false';
         $params['key'] = urlencode($this->apiKey);
 
@@ -37,9 +35,29 @@ class GooglePlaces extends Base
         $content = json_decode($responseBody);
 
         if ($content->status == 'OK' || $content->status == 'ZERO_RESULTS') {
-            return $content->results;
+
+            $sorted = array();
+
+            foreach ($content->results as &$result) {
+                $result->distance = \Helper\Location::distance($location['lat'], $location['lng'], $result->geometry->location->lat, $result->geometry->location->lng);
+                $sorted[] = $result;
+            }
+
+            usort($sorted, function($a, $b) {
+
+                if ($a === $b) {
+                    return 0;
+                }
+
+                return ($a->distance < $b->distance) ? -1 : 1;
+            });
+
+            return $sorted;
+
         } else {
+
             throw new \Exception("Service Unavailable (Google Places API said: '{$content->status}')", 503);
+
         }
     }
 }
