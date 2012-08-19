1295935c7c684a8c9447b363e17b93434d233402
Emran Hasan <phpfour@gmail.com>
2012-08-19 11:48:16 +0600

    Added cron running script.

---
+++ service/bin/cron.php
@@ -0,0 +1,11 @@
+<?php
+
+$client= new GearmanClient();
+$client->addServer('127.0.0.1');
+$client->setCompleteCallback(function($task){
+    echo $task->jobHandle() . " = " . $task->data(), PHP_EOL;
+    flush();
+});
+
+$task = $client->addTask("fetch_external_location", json_encode(array()));
+$client->runTasks();
