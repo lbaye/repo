241134e74fc108c2a5043e4d2ba721045fa4cb03
Abdullah Md. Zubair <raihan.zbr@gmail.com>
2012-09-11 22:03:43 +0600

    user basic profile
    user basic profile

---
+++ iphone/SocialMaps/UserBasicProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserBasicProfileViewController.h
+//  SocialMaps
+//
+//  Created by Abdullah Md. Zubair on 9/11/12.
+//  Copyright (c) 2012 Genweb2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserBasicProfileViewController : UIViewController
+
+@end
