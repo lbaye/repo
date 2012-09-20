5e4e919866c6d085eca467115e7ffd43ece8be26
Warif Rishi <rishi420@gmail.com>
2012-09-20 14:13:58 +0600

    Show alert for friend request

--- iphone/SocialMaps/RestClient.m
+++ iphone/SocialMaps/RestClient.m
@@ -3973,9 +3973,10 @@
         
         if (responseStatus == 200 || responseStatus == 201) {
             NSLog(@"sendFriendRequest successful:status=%d", responseStatus);
-            //[[NSNotificationCenter defaultCenter] postNotificationName:NOTIF_REG_DONE object:aUser];
+            [UtilityClass showAlert:@"" :@"Friend request sent"];
         } else {
             NSLog(@"sendFriendRequest unsuccessful:status=%d", responseStatus);
+            [UtilityClass showAlert:@"" :@"Friend request previously sent to this user."];
             //[[NSNotificationCenter defaultCenter] postNotificationName:NOTIF_REG_DONE object:nil];
         }
         [jsonParser release], jsonParser = nil;
@@ -3985,6 +3986,7 @@
     // Handle unsuccessful REST call
     [request setFailedBlock:^{
         NSLog(@"sendFriendRequest failed: unknown error");
+        [UtilityClass showAlert:@"" :@"Failed to send friend request"];
         //[[NSNotificationCenter defaultCenter] postNotificationName:NOTIF_REG_DONE object:nil];
     }];
     
