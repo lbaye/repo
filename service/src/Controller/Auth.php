1f7cd112720558a81d118fcab6e4c8928e6c8f94
Rafiq <islam.rafiqul@genweb2.com>
2012-09-24 19:45:47 +0600

    required fields must not be empty

--- service/src/Controller/Auth.php
+++ service/src/Controller/Auth.php
@@ -41,7 +41,7 @@ class Auth extends Base
 
         try {
 
-            if (empty($data['firstName']) || empty($data['lastName']) || empty($data['password']) || empty($data['avatar'])) {
+            if(empty($data['firstName']) || empty($data['lastName']) || empty($data['password']) || empty($data['avatar'])) {
                 $this->response->setContent(json_encode(array('message' => "Required field must not be empty!")));
                 $this->response->setStatusCode(Status::NOT_ACCEPTABLE);
                 return $this->response;
