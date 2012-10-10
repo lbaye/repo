b7bc3480b3714d029280357008e19aa59adc7c7c
Abdullah Md. Zubair <raihan.zbr@gmail.com>
2012-10-10 17:28:08 +0600

    Friendship status and registration media on list view

--- iphone/SocialMaps/LocationItemPeople.m
+++ iphone/SocialMaps/LocationItemPeople.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 Genweb2. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "LocationItemPeople.h"
 #import "Constants.h"
 
@@ -22,6 +23,9 @@
     //UILabel *lblDist     = (UILabel*) [cell viewWithTag:2004];
     //UIView *line         = (UIView*) [cell viewWithTag:2005];
     UITextView   *txtMsg = (UITextView*) [cell viewWithTag:2006];  
+    UIImageView *regMedia = (UIImageView*) [cell viewWithTag:20012];
+    UIButton *frndButton = (UIButton*) [cell viewWithTag:20016];
+    
     if (txtMsg == nil) {
         CGSize msgStringSize = [userInfo.statusMsg sizeWithFont:[UIFont fontWithName:@"Helvetica" size:kSmallLabelFontSize]];
         CGFloat msgRows = ceil(msgStringSize.width/tv.frame.size.width/2);
@@ -31,8 +35,8 @@
 //                                     lblName.frame.origin.y+lblName.frame.size.height+5,
 //                                     tv.frame.size.width/2, msgHeight);
         CGRect msgFrame = CGRectMake(80, 
-                                     lblName.frame.origin.y+lblName.frame.size.height+5,
-                                     tv.frame.size.width/2, msgHeight);
+                                     lblName.frame.origin.y+lblName.frame.size.height,
+                                     250, 25);
 
         txtMsg = [[[UITextView alloc] initWithFrame:msgFrame] autorelease];
 		txtMsg.tag = 2006;
@@ -40,15 +44,71 @@
 		txtMsg.font = [UIFont fontWithName:@"Helvetica" size:kSmallLabelFontSize];
 		txtMsg.backgroundColor = [UIColor clearColor];
 		[txtMsg setTextAlignment:UITextAlignmentLeft];
-        if (userInfo.statusMsg) 
-        {
-            txtMsg.text = userInfo.statusMsg;
-        }
         NSLog(@"people.statusMsg %@ %lf  %lf",userInfo.statusMsg, msgFrame.origin.x, msgFrame.origin.y);
         txtMsg.userInteractionEnabled = FALSE;
         [cell.contentView addSubview:txtMsg];
+                
+    }
+    if (userInfo.statusMsg) 
+    {
+        txtMsg.text = userInfo.statusMsg;
+    }
+    else
+    {
+        txtMsg.text=@"";
+    }
+
+    
+    if (regMedia==nil) 
+    {
+        regMedia=[[UIImageView alloc] initWithFrame:CGRectMake(85,20,20,20)];
+        regMedia.layer.borderColor=[[UIColor lightTextColor] CGColor];
+        regMedia.userInteractionEnabled=YES;
+        regMedia.layer.borderWidth=1.0;
+        regMedia.layer.masksToBounds = YES;
+        [regMedia.layer setCornerRadius:5.0];
+        regMedia.tag=20012;
+        [cell.contentView addSubview:regMedia];
+    }
+    if ([userInfo.regMedia isEqualToString:@"fb"]) 
+    {
+        NSLog(@"reg media fb %@",[UIImage imageNamed:@"icon_facebook.png"]);
+        regMedia.image=[UIImage imageNamed:@"icon_facebook.png"];
+    }
+    else
+    {
+        regMedia.image=[UIImage imageNamed:@"sm_icon@2x.png"];
     }
     
+    if (frndButton==nil) 
+    {
+        frndButton=[UIButton buttonWithType:UIButtonTypeCustom];
+        frndButton.frame= CGRectMake(40,66,35,20);
+        frndButton.layer.borderColor=[[UIColor lightTextColor] CGColor];
+        frndButton.userInteractionEnabled=NO;
+        frndButton.layer.borderWidth=1.0;
+        frndButton.layer.masksToBounds = YES;
+        [frndButton.layer setCornerRadius:5.0];
+        [frndButton setBackgroundImage:[UIImage imageNamed:@"checkbox_unchecked.png"] forState:UIControlStateNormal];
+        [frndButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica" size:10]];
+        [frndButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        frndButton.tag=20016;
+        
+        
+        [cell.contentView addSubview:frndButton];
+    }
+    if ([userInfo.friendshipStatus isEqualToString:@"friend"]==TRUE)
+    {
+        frndButton.hidden=NO;
+        [frndButton setTitle:@"friends" forState:UIControlStateNormal];
+        NSLog(@"is hidden:NO userInfo.friendshipStatus %@",userInfo.friendshipStatus);
+    }
+    else
+    {
+        frndButton.hidden=YES;
+        NSLog(@"is hidden:YES userInfo.friendshipStatus %@",userInfo.friendshipStatus);
+        [frndButton setTitle:@"Non-Friends" forState:UIControlStateNormal];
+    } 
 	// Message
     
     // Debug
