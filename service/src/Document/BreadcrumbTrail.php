d684a7d9cf5ad24caa142a935a7a8208c1a6ef37
Zunaid Kazi <zunaid@kazi.net>
2012-10-18 19:32:46 -0700

    BreadCrumb Trail Model, inherited from Trail document

---
+++ service/src/Document/BreadcrumbTrail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Respect\Validation\Validator;
+
+use Document\User as User;
+use Document\Location as Location;
+
+/**
+ * @ODM\Document(collection="breadcrumbTrails",repositoryClass="Repository\TrailRepo")
+ */
+class BreadcrumbTrail extends Trail
+{
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        try {
+            Validator::create()->notEmpty()->assert($this->getName());
+
+        } catch (\InvalidArgumentException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
