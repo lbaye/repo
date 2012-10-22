326f700f0f25af8072329e6b6c3df6f8f17fb9bc
az7arul <az7arul@gmail.com>
2012-10-22 18:12:41 +0600

    updated the reverselookup methods to be more modular

--- service/src/Repository/CachedDataRepo.php
+++ service/src/Repository/CachedDataRepo.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: azhar
- * Date: 10/20/12
- * Time: 7:04 PM
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Repository;
 
@@ -17,23 +10,15 @@ class CachedDataRepo extends Base {
         return $type . ':' . $id;
     }
 
-    public function put($id, $data, $type, CachedData $instance = null) {
-        if (is_null($instance)) {
-            $instance = new \Document\CachedData();
-        }
-
-        $instance->setId($this->buildId($id, $type));
-        $instance->setData($data);
-        $instance->setType($type);
-
+    public function put(CachedData $instance) {
         $this->dm->persist($instance);
         $this->dm->flush();
 
-        return $instance;
+        return true;
     }
 
     public function get($id){
-        $cached_data = $this->findOneBy(array('id' => $id));
+        $cached_data = $this->find($id);
         return is_null($cached_data) ? false : $cached_data;
     }
 
