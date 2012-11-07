9bea2e9b1053aaa4a22444402a1556ab9dc2d135
Rafiq <islam.rafiqul@genweb2.com>
2012-11-07 13:54:37 +0600

    removed friend from all of my circle then again add

--- service/src/Controller/User.php
+++ service/src/Controller/User.php
@@ -757,6 +757,7 @@ class User extends Base {
         try {
 
             $circleData = $this->request->request->all();
+            $this->userRepository->removeFriendFromCircle($id, $circleData);
 
             $this->userRepository->addFriendToMultipleCircle($id, $circleData);
 
