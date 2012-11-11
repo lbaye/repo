94fbf0f5cd3652adfb1c21aa6769a86de1d62f1c
we4tech <hasan83bd@gmail.com>
2012-11-11 11:42:03 +0600

    Increased people quota, now it will load 2000 people

--- service/src/Controller/Search.php
+++ service/src/Controller/Search.php
@@ -13,7 +13,7 @@ class Search extends Base {
     /**
      * Total number of users to return
      */
-    const PEOPLE_THRESHOLD = 100;
+    const PEOPLE_THRESHOLD = 2000;
 
     /**
      * Km to radius (km / 111.2)
