6d9f8a34cb3a0bc2ee5ba15a2d5a47a6d7437cef
Abdullah Md. Zubair <raihan.zbr@gmail.com>
2012-12-02 11:00:22 +0600

    image resized proportionally

--- iphone/SocialMaps/PhotoPicker.m
+++ iphone/SocialMaps/PhotoPicker.m
@@ -128,6 +128,7 @@
     NSLog(@"After image selection: width=%f, height=%f",
           selectedImage.size.width, selectedImage.size.height);
     UIImage *scaledImage = [self scaleImage:selectedImage toSize:imgSize];
+    scaledImage=[self imageByScalingProportionallyToSize:scaledImage];
     NSLog(@"After image scaled: width=%f, height=%f",
           scaledImage.size.width, scaledImage.size.height);    
     regPhoto = scaledImage;
@@ -157,6 +158,67 @@
     UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return scaledImage;
+}
+
+- (UIImage *)imageByScalingProportionallyToSize:(UIImage *)sourceImage
+{
+    
+    UIImage *newImage = nil;
+    CGSize targetSize=CGSizeMake(sourceImage.size.width/2, sourceImage.size.height/2);
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor < heightFactor) 
+            scaleFactor = widthFactor;
+        else
+            scaleFactor = heightFactor;
+        
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        
+        if (widthFactor < heightFactor) {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5; 
+        } else if (widthFactor > heightFactor) {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    
+    // this is actually the interesting part:
+    
+    UIGraphicsBeginImageContext(targetSize);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width  = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    if(newImage == nil) NSLog(@"could not scale image");
+    
+    
+    return newImage ;
 }
 
 //
