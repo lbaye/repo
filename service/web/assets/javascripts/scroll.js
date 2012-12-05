03cc21c82aff2b7226a696ee61830dcd7bc9af3b
az7arul <az7arul@gmail.com>
2012-12-05 15:34:09 +0600

    refactored variable names

--- service/web/assets/javascripts/scroll.js
+++ service/web/assets/javascripts/scroll.js
@@ -1,36 +1,36 @@
 $(window).scroll(function () {
 
-    var body = $('.content');
-    var userId = body.attr("userId");
-    var baseUrl = body.attr("baseUrl");
-    var authToken = body.attr("authToken");
+    var container = $('.content');
+    var userId = container.attr("userId");
+    var baseUrl = container.attr("baseUrl");
+    var authToken = container.attr("authToken");
 
     var enableScroll = function () {
-        body.attr("scroll", "yes");
+        container.attr("scroll", "yes");
     }
 
     var success = function (res) {
         $('.content ul').append(res);
         $('img.lazy').lazyload();
         var new_num = parseInt($('.events').length);
-        var old_num = parseInt(body.attr('count'));
-        body.attr("count", new_num);
+        var old_num = parseInt(container.attr('count'));
+        container.attr("count", new_num);
         if (new_num > (old_num + 4)) {
             window.setTimeout(enableScroll, 2000);
         }
     }
-    var body_attr = body.attr("scroll");
-    var turnOff = body.attr("turnOff");
+    var scroll_attr = container.attr("scroll");
+    var turnOff = container.attr("turnOff");
 
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
 
-        if (body_attr == "yes") {
-            var elem = parseInt($('.events').length);
-            body.attr("scroll", "no");
+        if (scroll_attr == "yes") {
+            var offset = parseInt($('.events').length);
+            container.attr("scroll", "no");
             $.ajax({
                 type:'get',
                 url:baseUrl + "/" + userId + "/newsfeed.html?authToken=" + authToken,
-                data:{offset:elem},
+                data:{offset: offset},
                 success:success
             });
         }
